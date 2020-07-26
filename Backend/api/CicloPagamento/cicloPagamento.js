80ebc478c3d72eb6a0ee9953e2412e69bb7655a2
Felipe Trevizan <felipeptrevizan@yahoo.com.br>
2020-07-26 15:52:08 -0300

    Testando a API de Ciclo de Pagamentos (Parte 1)

--- Backend/api/CicloPagamento/cicloPagamento.js
+++ Backend/api/CicloPagamento/cicloPagamento.js
@@ -8,14 +8,14 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    value: {type: Number, min: 0, required: true},
+    value: {type: Number, min: 0, required: [true, 'Informe o valor do débito!']},
     status: {type: String, required: false, uppercase: true,
     enum: ['PAGO','PENDENTE','AGENDADO']}
 })
 
 const billingCycleSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    moth: {type: Number, required: true},
+    month: {type: Number, required: true},
     year: {type: Number, min: 1970, max: 2100, required: true},
     credits: [creditSchema],
     debts: [debtSchema]
--- Backend/config/routes.js
+++ Backend/config/routes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 
 module.exports = function(server) {
+    
  const router = express.Router()
- server.use('/api', router)   
+ server.use('/api', router)
+
+ const billingCycleService = require('../api/CicloPagamento/cicloPagamentoServico')
+ billingCycleService.register(router, '/billingCycles')
+
 }
