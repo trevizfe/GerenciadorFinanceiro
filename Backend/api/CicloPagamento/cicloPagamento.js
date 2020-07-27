d1cefc777f013497925d31860424067216ade3a8
Felipe Trevizan <felipeptrevizan@yahoo.com.br>
2020-07-26 21:14:47 -0300

    Testando a API de Ciclo de Pagamentos (Parte 2)

--- Backend/api/CicloPagamento/cicloPagamento.js
+++ Backend/api/CicloPagamento/cicloPagamento.js
@@ -15,7 +15,7 @@ const debtSchema = new mongoose.Schema({
 
 const billingCycleSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    month: {type: Number, required: true},
+    month: {type: Number, min: 1, max: 12, required: true},
     year: {type: Number, min: 1970, max: 2100, required: true},
     credits: [creditSchema],
     debts: [debtSchema]
--- Backend/api/CicloPagamento/cicloPagamentoServico.js
+++ Backend/api/CicloPagamento/cicloPagamentoServico.js
@@ -1,5 +1,6 @@
 const BillingCycle = require('./cicloPagamento')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
 
 module.exports = BillingCycle
