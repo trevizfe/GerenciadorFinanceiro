ae71f7d6b0252732cd9704b27ed8243b04c99cd8
Felipe Trevizan <felipeptrevizan@yahoo.com.br>
2020-07-26 21:29:39 -0300

    Serviço Contador (count) de Ciclo de Pagamentos

--- Backend/api/CicloPagamento/cicloPagamentoServico.js
+++ Backend/api/CicloPagamento/cicloPagamentoServico.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./cicloPagamento')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', function(req, res, next){
+    BillingCycle.count(function(error, value){
+        if(error){
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
